ac6bd88c072b7aeeca6fba682223f594f2c52fb4
oyasumi <k.anuchit@kkumail.com>
2021-05-16 16:16:46 +0700

    Create AuthController and add api route:

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+    }
+
+    public function signup(Request $request)
+    {
+    }
+
+    public function logout(Request $request)
+    {
+    }
+
+    public function user(Request $request)
+    {
+    }
+}
